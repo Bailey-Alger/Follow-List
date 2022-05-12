265e3f6c80fed8e43a209e8bac5d7f4b9af98ed6
Bailey Alger <balger455@gmail.com>
2022-05-12 17:43:13 -0400

    first commit

---
+++ popup.js
@@ -0,0 +1,38 @@
+// Initialize button with user's preferred color
+/*let changeColor = document.getElementById("changeColor");
+
+chrome.storage.sync.get("color", ({ color }) => {
+    changeColor.style.backgroundColor = color;
+});
+
+// When the button is clicked, inject setPageBackgroundColor into current page
+changeColor.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: setPageBackgroundColor,
+    });
+});
+
+// The body of this function will be executed as a content script inside the
+// current page
+function setPageBackgroundColor() {
+    chrome.storage.sync.get("color", ({ color }) => {
+        document.body.style.backgroundColor = color;
+    });
+}*/
+
+let currentFollow = document.getElementById("currentFollow");
+
+currentFollow.addEventListener("focus", async () => {
+
+});
+
+function checkForFollow() {
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        let url = tabs[0].url;
+        // use `url` here inside the callback because it's asynchronous!
+    });
+    ;
+}
